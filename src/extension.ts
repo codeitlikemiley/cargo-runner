109f40ab4ca896cd8f08d25dc392057e9bb1ad47
Uriah <codeitlikemiley@gmail.com>
2024-01-02 05:17:06 +0800

    ready to be used

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,8 @@
 import * as vscode from 'vscode';
-import { getMakefile } from './get_makefile';
-import { isMakefileValid } from './is_makefile_valid';
-import { checkCrateType } from './check_crate_type';
-import { isFileInTestContext } from './is_file_in_test_context';
-import { getBin } from './get_bin';
-import { getPackage } from './get_package';
-import { getTestName } from './get_test_name';
-import { tests } from './tests';
 import { exec } from './exec';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.tests', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No active editor found.');
-			return;
-		}
-
-		const filePath = editor.document.uri.fsPath;
-		const packageName = await getPackage(filePath);
-		const binName = await getBin(filePath);
-		const command = await tests(filePath, packageName, binName);
-		if (command) {
-			vscode.window.showInformationMessage(`Command: ${command}`);
-		} else {
-			vscode.window.showInformationMessage('Not a test.');
-		}
-	}));
-
-	// add `cargo-runner.exec` command
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.exec', async () => {
+    context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.exec', async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			vscode.window.showErrorMessage('No active editor found.');
@@ -38,87 +10,14 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 		const command = await exec();
 		if (command) {
-			vscode.window.showInformationMessage(`Command: ${command}`);
+			let terminal = vscode.window.activeTerminal;
+			if (!terminal) {
+				terminal = vscode.window.createTerminal(`Cargo Run Terminal`);
+			}
+			terminal.sendText(command);
+			terminal.show();
 		} else {
 			vscode.window.showInformationMessage('Cannot run.');
 		}
-
 	}));
-
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.checkCrateType', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const filePath = editor.document.uri.fsPath;
-			const crateType = await checkCrateType(filePath);
-			vscode.window.showInformationMessage(`Crate Type: ${crateType || 'None'}`);
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getBin', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const filePath = editor.document.uri.fsPath;
-			const binName = await getBin(filePath);
-			vscode.window.showInformationMessage(`Binary Name: ${binName || 'None'}`);
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getMakefile', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No active editor found.');
-			return;
-		}
-
-		const filePath = editor.document.uri.fsPath;
-		const makefilePath = await getMakefile(filePath);
-		vscode.window.showInformationMessage(`Makefile Path: ${makefilePath || 'None'}`);
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getPackage', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const filePath = editor.document.uri.fsPath;
-			const packageName = await getPackage(filePath);
-			vscode.window.showInformationMessage(`Package Name: ${packageName || 'None'}`);
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getTestName', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const filePath = editor.document.uri.fsPath;
-			const testName = await getTestName(filePath);
-			vscode.window.showInformationMessage(`Test Function Name: ${testName || 'None'}`);
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.isFileInTestContext', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (!editor) {
-			vscode.window.showErrorMessage('No active editor found.');
-			return;
-		}
-		const isInTestContext = await isFileInTestContext();
-		vscode.window.showInformationMessage(`Is in Test Context: ${isInTestContext}`);
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.isMakefileValid', async () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const filePath = editor.document.uri.fsPath;
-			const makefilePath = await getMakefile(filePath);
-			const isValid = makefilePath ? isMakefileValid(makefilePath) : false;
-			vscode.window.showInformationMessage(`Is Makefile Valid: ${isValid}`);
-		}
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.isWorkspace', () => {
-		const isWorkspace = vscode.workspace.workspaceFolders !== undefined;
-		vscode.window.showInformationMessage(`Is Workspace: ${isWorkspace}`);
-	}
-	));
 }
-
-

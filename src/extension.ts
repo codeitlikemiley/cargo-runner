8bfa840b022f14ee93e2e5b67a9a740b4d0f67f1
Uriah <codeitlikemiley@gmail.com>
2024-01-02 01:20:56 +0800

    testing

--- src/extension.ts
+++ src/extension.ts
@@ -6,8 +6,32 @@ import { isFileInTestContext } from './is_file_in_test_context';
 import { getBin } from './get_bin';
 import { getPackage } from './get_package';
 import { getTestName } from './get_test_name';
+import { cargoRun } from './cargo_run';
+import { cargoNextest } from './cargo_nextest';
 
 export function activate(context: vscode.ExtensionContext) {
+
+	// add `cargo-runner.cargoNextest` command
+	// add `cargo-runner.cargoNextest` command
+	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.cargoNextest', async () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor found.');
+			return;
+		}
+
+		const filePath = editor.document.uri.fsPath;
+		const packageName = await getPackage(filePath);
+		const binName = await getBin(filePath);
+		const command = await cargoNextest(filePath, binName);
+		if (command) {
+			vscode.window.showInformationMessage(`Command: ${command}`);
+		} else {
+			vscode.window.showInformationMessage('Not a test.');
+		}
+	}));
+
+
 
 	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.checkCrateType', async () => {
 		const editor = vscode.window.activeTextEditor;
@@ -48,13 +72,22 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getTestName', () => {
-		const testName = getTestName();
-		vscode.window.showInformationMessage(`Test Function Name: ${testName || 'None'}`);
+	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.getTestName', async () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const filePath = editor.document.uri.fsPath;
+			const testName = await getTestName(filePath);
+			vscode.window.showInformationMessage(`Test Function Name: ${testName || 'None'}`);
+		}
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('cargo-runner.isFileInTestContext', async () => {
-		const isInTestContext = isFileInTestContext();
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor found.');
+			return;
+		}
+		const isInTestContext = await isFileInTestContext();
 		vscode.window.showInformationMessage(`Is in Test Context: ${isInTestContext}`);
 	}));
 

fab356049057c3eddd7e50efa7a3a5b42cad8eb1
Uriah <codeitlikemiley@gmail.com>
2024-01-23 18:13:59 +0800

    module path fix for bin name

--- src/get_module_path.ts
+++ src/get_module_path.ts
@@ -1,4 +1,4 @@
-export default function getModulePath(filePath: string, packageName: string): string {
+export default function getModulePath(filePath: string, packageName: string, binName: string|null): string {
     // Find the position of the package name in the file path
     const packageIndex = filePath.indexOf(`/${packageName}/src/`);
 
@@ -14,12 +14,20 @@ export default function getModulePath(filePath: string, packageName: string): st
         const formattedPath = modulePath.replace(/\//g, '::');
 
         // Remove the file extension (.rs)
-        const finalPath = formattedPath.replace('.rs', '');
+        let finalPath = formattedPath.replace('.rs', '');
 
         // if our final path has ::mod on the end, remove it
         if (finalPath.endsWith('::mod')) {
-            return finalPath.slice(0, -5);
+            finalPath =  finalPath.slice(0, -5);
         }
+        // remove the bin name from the path, can remove multiple element that matches the bin name
+        console.log(`get_module_path - Final path: ${finalPath}`);
+        if (finalPath) {
+            const pathSegments = finalPath.split('::');
+            const filteredSegments = pathSegments.filter(segment => segment !== binName);
+            finalPath = filteredSegments.join('::');
+        }
+        
 
         return finalPath;
     }
